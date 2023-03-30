708f88e2e4acfbd6da0335896c0bfbdf094425df
Jose Miguel Contro Azanza <111621479+jmcontro00@users.noreply.github.com>
2023-03-29 23:26:10 -0600

    Cambios de prototipo 1

---
+++ appFinalContro/agregar.swift
@@ -0,0 +1,20 @@
+//
+//  agregar.swift
+//  appFinalContro
+//
+//  Created by Amieva on 29/03/23.
+//
+
+import SwiftUI
+
+struct agregar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct agregar_Previews: PreviewProvider {
+    static var previews: some View {
+        agregar()
+    }
+}
---
+++ appFinalContro/buscar.swift
@@ -0,0 +1,20 @@
+//
+//  buscar.swift
+//  appFinalContro
+//
+//  Created by Amieva on 29/03/23.
+//
+
+import SwiftUI
+
+struct buscar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct buscar_Previews: PreviewProvider {
+    static var previews: some View {
+        buscar()
+    }
+}
